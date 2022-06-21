14b2054063c3e3c14277507fdb069556c3e52262
TomBooth <thomas.alexander.booth@gmail.com>
2022-06-21 13:32:28 +0100

    login almost complete need to space inputs

--- my-app/src/Login/index.js
+++ my-app/src/Login/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import FormInput from '../App/Components/InputTypeText/Input-Index'
-import Button from '../App/Components/Button/ButtonIndex'
-
+import GreenButton from '../App/Components/Button/GreenButtonIndex'
+import OrangeButton from '../App/Components/Button/OrangeButtonIndex'
+import '../App/App.css'
 const Login = () => {
     return (
        
@@ -9,10 +10,10 @@ const Login = () => {
         
         <header className='header'>
         <img className='our-logo' src="https://i.ibb.co/SJKYb1L/logov1-copy.png" alt="Bootcamper Social Logo"/>
-        <img className='profile-icon' src="https://i.ibb.co/zXrZDfm/Place-Holder-Profile-Pic.png" alt="Profile Photo or Initial Place Holder"/>
+        {/* <img className='profile-icon' src="https://i.ibb.co/zXrZDfm/Place-Holder-Profile-Pic.png" alt="Profile Photo or Initial Place Holder"/> */}
         </header>
         
-            
+           <div className='login-inputs'> 
            <h1 className="h1-styling">Username</h1>
            <FormInput name="username-input" />
            <h1 className="h1-styling">Password</h1>
@@ -20,12 +21,13 @@ const Login = () => {
            <br></br>
            <a className='forgotten-password-link' href="url">Forgotten Username or Password?</a>
            <br></br>
-           <Button className="green-button" buttonText={"Login"}/>
+           </div>
+           <GreenButton className="green-button" buttonText={"Login"}/>
            <br></br>
-           <Button className="orange-button" buttonText={"Create User"}/>
+           <OrangeButton className="orange-button" buttonText={"Create User"}/>
            <br></br>
-           <p>Dont want an Account? <br></br>Click Below.</p>
-           <Button className="green-button" buttonText={"Guest Login"}/>
+           <p id="guest-login-prompt">Dont want an Account? <br></br>Click Below.</p>
+           <GreenButton className="green-button" buttonText={"Guest Login"}/>
 
 
         </div>
