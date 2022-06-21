538b0343289faf978d051497f8f1f45a64048204
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2022-06-21 15:51:07 +0100

    Little Red Button

---
+++ my-app/src/App/Components/Button/LittleRedButtonindex.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+// import "./button.css";
+
+function LittleRedButton({ handleClick, buttonText }) {
+  return (
+      <button onClick={handleClick } className="little-red-button">
+    
+        {buttonText}
+      </button>
+  );
+}
+export default LittleRedButton;
