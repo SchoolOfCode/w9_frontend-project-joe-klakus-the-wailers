c493eef37888e9a751bf0ce96fb5c48ff79f59c6
JojokCreator <joeyk2k@hotmail.com>
2022-06-23 08:22:22 +0100

    login functional

--- my-app/src/App/index.js
+++ my-app/src/App/index.js
@@ -3,28 +3,48 @@ import Login from '../Login';
 import CreateUser from '../CreateUser';
 import CreateEvent from '../CreateEvent';
 import ProfilePage from '../ProfilePage';
-import ProfilePageLocked from '../ProfilePage/lockedprofileIndex';
 import MainPage from './Components/MainPage';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { jwtDecode } from "../App/jwt-decode.js";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [id, setId] = useState(0);
+  const [token, setToken] = useState('');
 
-  const toggleLogin = () => {
-    setIsLoggedIn(false)
-    console.log(isLoggedIn)
+  useEffect(() => {
+    //checks to see if the user is logged in ie. has a valid token
+  async function fetchRefreshToken() {
+    const res = await fetch('http://localhost:5000/refresh_token', {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      mode: "cors",
+      credentials: "include",
+    });
+    //saves the encrypted token to state (bad practice i think needs to be changed later)
+    const jsonResponse = await res.json();
+    setToken(jsonResponse.accessToken)
+    // decodes the token to get the user id
+    if (jsonResponse.accessToken){
+    const jwtDecoded = jwtDecode(jsonResponse.accessToken)
+    setId(jwtDecoded.user_id)
+    return jsonResponse.refreshToken;
+    }
   }
-//comment here
+  //runs the function
+  fetchRefreshToken()
+},[]);
+
 return (
   <div className="app">
   <BrowserRouter>
   <Routes>
-  <Route index element={isLoggedIn ? <Login onClickLogin={toggleLogin} text=""/>: <MainPage></MainPage>}/>
+  <Route index element={(!token) ? <Login text=""/> : <MainPage></MainPage>}/>
   <Route path="/newu" element={<CreateUser text=""/> }></Route>
-  <Route path="/newe" element={<CreateEvent text=""/> }></Route>
-  <Route path="/updateu" element={<ProfilePage text=""/> }></Route>
-  {/* <ProfilePageLocked text=""/> */} */}
+  <Route path="/newe" element={<CreateEvent id={id} text=""/> }></Route>
+  <Route path="/updateu" element={<ProfilePage id={id} text=""/>}></Route>
+  <Route path="/main" element={<MainPage text=""/>}></Route>
   </Routes>
   </BrowserRouter>
   </div>
